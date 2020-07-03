dbc413a45fdd60338152fd8f20d8e98bdec1c65b
cfmago <cfm.agostinho@gmail.com>
2020-07-03 09:32:16 +0100

    game now ends at pact with the devil, but he keeps asking if you say no

--- game-sample-code-thursday-v.py
+++ game-sample-code-thursday-v.py
@@ -30,7 +30,7 @@ tv = {
 
 devil = {
     "name": "the devil",
-    "type": "door",
+    "type": "character",
 } 
 
 key_a = {
@@ -108,6 +108,7 @@ living_room = {
     "type": "room",
 }
 
+
 underworld = {
     "name": "underworld",
     "type": "room",
@@ -123,8 +124,8 @@ all_doors = [door_a, door_b, door_c, tv]
 
 object_relations = {
     "game room": [couch, piano, door_a], # These are the objects in game room
-    "bedroom 1": [door_a, door_b, door_c, queen_bed],
-    "bedroom 2": [door_b, double_bed, dresser],
+    "bedroom 1": [queen_bed, door_a, door_b, door_c],
+    "bedroom 2": [double_bed, dresser, door_b],
     "living room": [dining_table, tv],
     "underworld":[devil],
     "piano": [key_a],
@@ -132,14 +133,15 @@ object_relations = {
     "double bed": [key_c],
     "dresser": [remote_control],
     "door to bedroom 1": [door_a], 
-    "door a": [game_room, bedroom_1], # bedroom_1 replaces outside 
+    "door a": [game_room, bedroom_1], 
     "door to bedroom 2": [door_b], 
     "door b": [bedroom_1, bedroom_2], 
     "door to living room": [door_c], 
     "door c": [bedroom_1, living_room], 
     "door to outside": [tv], 
     "tv": [living_room, underworld], 
-    "devil":[underworld,bedroom_2,game_room]
+#    "devil":[underworld, game_room]
+#    "devil":[underworld, outside]
 } 
 
 # define game state. Do not directly change this dict. 
@@ -165,31 +167,14 @@ def start_game():
     """
     print("You wake up on a couch and find yourself in a strange house with no windows which you have never been to before. You don't remember why you are here and what had happened before. You feel some unknown danger is approaching and you must get out of the house, NOW!")
     play_room(game_state["current_room"])
-
-"""def play_room(room):
-    
-
-    game_state["current_room"] = room
-    if(game_state["current_room"] == game_state["target_room"]):
-        print("Congrats! You escaped the room!")
-    else:
-        print("You are now in " + room["name"])
-        intended_action = input("What would you like to do? Type 'explore' or 'examine'?").strip()
-        if intended_action == "explore":
-            explore_room(room)
-            play_room(room)
-        elif intended_action == "examine":
-            examine_item(input("What would you like to examine?").strip())
-        else:
-            print("Not sure what you mean. Type 'explore' or 'examine'.")
-            play_room(room)
-        linebreak()"""
-
+        
+        
 def play_room(room):
     """
     Play a room. First check if the room being played is the target room.
-    If it is, the game will end with success. Otherwise, let player either
+    If it is, the game will end with success. Otherwise, let player either 
     explore (list all items in this room) or examine an item found here.
+    """
     """
     game_state["current_room"] = room
     if(game_state["current_room"] == game_state["target_room"]):
@@ -198,15 +183,36 @@ def play_room(room):
         items = [i["name"] for i in object_relations[room["name"]]]
         print("This is " + room["name"] + ". In this room there's a " + ", ".join(items))
         examine_item(input("What would you like to examine?").strip())
+        
 
-
-"""def explore_room(room):
-    
-    #Explore a room. List all items belonging to this room.
-    
-    #items = [i["name"] for i in object_relations[room["name"]]]
-    #print("You explore the room. This is " + room["name"] + ". You find " + ", ".join(items))
 """
+    game_state["current_room"] = room
+    current_room = game_state["current_room"]
+    
+    if(game_state["current_room"] == game_state["target_room"]):
+        print("Congrats! You escaped the room!")
+    
+    elif(game_state["current_room"] == underworld):
+        print("In this room you find the devil. He asks you 'Do you want to make a pact?'")
+    
+    #    if(next_room and input("Enter 'yes' or 'no'").strip() == 'yes'):
+        if input("Enter 'yes' or 'no'").strip() == 'yes':
+            print("You find a unicorn. Hop on it and fly away. See you in another!")
+            
+        elif input("Enter 'yes' or 'no'").strip() == 'no':
+            print("Groundhog day!")
+            start_game()
+    
+        else:
+            play_room(current_room) 
+        
+    else:
+        items = [i["name"] for i in object_relations[room["name"]]]
+        print("This is " + room["name"] + ". In this room there's a " + ", ".join(items))
+        examine_item(input("What would you like to examine?").strip())
+        
+    
+            
 
 def get_next_room_of_door(door, current_room):
     """
@@ -217,7 +223,8 @@ def get_next_room_of_door(door, current_room):
     for room in connected_rooms:
         if(not current_room == room):
             return room
-
+        
+        
 def examine_item(item_name):
     """
     Examine an item which can be a door or furniture.
@@ -229,55 +236,48 @@ def examine_item(item_name):
     play either the current or the next room depending on the game state
     to keep playing.
     """
-    current_room = game_state["current_room"]
-    next_room = ""
-    output = None
- 
-        
+    
     current_room = game_state["current_room"]
     next_room = ""
     output = None
     
-    if(current_room["name"] == "underworld"):
-        output = "Do you want to make a pact?"
-        next_room = get_next_room_of_door(item, current_room)
-    else:
-        for item in object_relations[current_room["name"]]:
-            if(item["name"] == item_name):
-                output = "You examine " + item_name + ". "
+    for item in object_relations[current_room["name"]]:
+        if(item["name"] == item_name):
+            output = "You examine " + item_name + ". "
+            if(item["name"] == "tv"):
+                output += "You turn on the tv. It opens a portal"
+                next_room = get_next_room_of_door(item, current_room) 
+            
+            elif(item["type"] == "door"):
+                have_key = False
                 
-                if(item["name"] == "tv"):
-                     output += "you turn on the tv. It opens a portal"
-                     next_room = get_next_room_of_door(item, current_room)
-                
-                elif(item["type"] == "door"):
-                    have_key = False
-                    for key in game_state["keys_collected"]:
-                        if(key["target"] == item):
-                            have_key = True
-                    if(have_key):
-                        output += "You unlock it with a key you have."
-                        next_room = get_next_room_of_door(item, current_room)
-                    else:
-                        output += "It is locked but you don't have the key."
+                for key in game_state["keys_collected"]:
+                    if(key["target"] == item):
+                        have_key = True
+                if(have_key):
+                    output += "You unlock it with a key you have."
+                    next_room = get_next_room_of_door(item, current_room)
                 else:
-                    if(item["name"] in object_relations and len(object_relations[item["name"]])>0):
-                        item_found = object_relations[item["name"]].pop()
-                        game_state["keys_collected"].append(item_found)
-                        output += "You find " + item_found["name"] + "."
-                    else:
-                        output += "There isn't anything interesting about it."
-                print(output)
-                break
+                    output += "It is locked but you don't have the key."
+            
+            else:
+                if(item["name"] in object_relations and len(object_relations[item["name"]])>0):
+                    item_found = object_relations[item["name"]].pop()
+                    game_state["keys_collected"].append(item_found)
+                    output += "You find " + item_found["name"] + "."
+                else:
+                    output += "There isn't anything interesting about it."
+            print(output)
+            break
 
     if(output is None):
-        print("The item you requested is not found in the current room.")
-    
+        print("The item you requested is not found in the current room.") 
+   
     if(next_room and input("Do you want to go to the next room? Enter 'yes' or 'no'").strip() == 'yes'):
-        play_room(next_room)
+        play_room(next_room)    
+    
     else:
-        play_room(current_room)
-        
+        play_room(current_room)        
         
         
 game_state = INIT_GAME_STATE.copy()
